cde349c855cfde25e293fdfe53d0030f50d89645
simonaditia <smnsitumeang@gmail.com>
2023-09-18 23:30:39 +0700

    Add locationOfficialName & remove unused codes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
 
-// import 'dotenv/config'
 const express = require("express");
 const fetch = require("node-fetch")
 const app = express()
@@ -15,27 +14,17 @@ app.listen(PORT, () => {
 })
 
 app.get("/location", async (req, res) => {
-    // res.send("Hello World!")
-    // const fetchApi = await fetch("https://weatherapi-com.p.rapidapi.com/forecast.json?q=bekasi", {
-    // const fetchApi = await fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/countries?offset=0&limit=50", {
-    //     "method": "GET",
-    //     "headers": {
-    //         'X-RapidAPI-Key': '9f2011d41fmsh023eb76e905ac08p17146cjsnaa12786b5190',
-    //         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    //     }
-    // })
     const fetchApiLocation = await fetch("https://restcountries.com/v3.1/all", {
         "method": "GET"
     })
 
     const locationNameResponse = await fetchApiLocation.json()
-    // console.log(locationNameResponse)
-    // const data = res.json(locationNameResponse)
 
-    let locationName = locationNameResponse[Math.floor(Math.random() * locationNameResponse.length)].name.common
-    console.log(locationName)
+    let locationName = locationNameResponse[Math.floor(Math.random() * locationNameResponse.length)].name
+    let locationNameCommon = locationName.common
+    let locationNameOfficial = locationName.official
 
-    const fetchApiLocationImage = await fetch(`https://bing-image-search1.p.rapidapi.com/images/search?q=${locationName}&count=20`, {
+    const fetchApiLocationImage = await fetch(`https://bing-image-search1.p.rapidapi.com/images/search?q=${locationNameCommon}&count=20`, {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': process.env.API_KEY,
@@ -44,24 +33,11 @@ app.get("/location", async (req, res) => {
     })
     const locationImageResponse = await fetchApiLocationImage.json()
     let locationImage = locationImageResponse.value[Math.floor(Math.random() * locationImageResponse.value.length)]
-    console.log(locationImage)
-    // console.log(fetchApiLocationImage)
-    // res.json(locationImage)
+
     res.json({
-        name: locationName,
+        name: locationNameCommon,
+        nameOfficial: locationNameOfficial,
         nameAlt: locationImage.name,
         thumbnailUrl: locationImage.thumbnailUrl
     })
 })
-
-// app.get("/locationImage", async (req, res) => {
-//     const fetchApi = await fetch("https://bing-image-search1.p.rapidapi.com/images/search?q=indonesia&count=20", {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': process.env.API_KEY,
-//             'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
-//         }
-//     })
-//     const locationImage = await fetchApi.json()
-//     res.json(locationImage)
-// })
--- public/script.js
+++ public/script.js
@@ -7,48 +7,30 @@ document.querySelector("#btnLoad").addEventListener("click", () => {
     if (document.querySelector("#locationImage") !== null) {
         document.querySelector("#locationImage").remove()
     }
+    if (document.querySelector("#locationOfficialName") !== null) {
+        document.querySelector("#locationOfficialName").remove()
+    }
     getLocation()
 })
 
 
 async function getLocation() {
-    // await fetch("/weather").then(response => response).then(console.log)
     const res = await fetch("http://localhost:3000/location")
     const data = await res.json()
-    // let locationName = data.data[Math.floor(Math.random() * data.data.length)].name
-    // let locationName = data[Math.floor(Math.random() * data.length)].name.common
-    // console.log(locationName)
+
     let locationNameDiv = document.createElement("div")
     locationNameDiv.id = "locationName"
     locationNameDiv.textContent = data.name
     document.querySelector("#locationWrapper").appendChild(locationNameDiv)
-    // console.log(data[0])
-    // console.log(data.length)
+
     let img = document.createElement("img")
     img.id = "locationImage"
     img.src = data.thumbnailUrl
     img.alt = data.nameAlt
     document.querySelector("#locationWrapper").appendChild(img)
+
+    let locationOffcialNameDiv = document.createElement("div")
+    locationOffcialNameDiv.id = "locationOfficialName"
+    locationOffcialNameDiv.textContent = data.nameOfficial
+    document.querySelector("#locationWrapper").appendChild(locationOffcialNameDiv)
 }
-
-// async function getLocationImage() {
-//     const res = await fetch("http://localhost:3000/location")
-//     const data = await res.json()
-// console.log(data.value[0].thumbnailUrl)
-// console.log(data.value[Math.floor(Math.random() * data.value.length)].thumbnailUrl)
-// console.log(data)
-// let locationImage = data.value[Math.floor(Math.random() * data.value.length)]
-// let locationImageUrl = locationImage.thumbnailUrl
-// let locationAlt = locationImage.name
-// console.log(locationImage)
-
-// if (document.querySelector("#locationImage") !== null) {
-//     document.querySelector("#locationImage").remove()
-// }
-
-//     let img = document.createElement("img")
-//     img.id = "locationImage"
-//     img.src = data.thumbnailUrl
-//     img.alt = data.nameAlt
-//     document.querySelector("#locationWrapper").appendChild(img)
-// }
